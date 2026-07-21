1b3372ff02038c930181e43074f03a1c37c335b2
Devang <bdevang3993@gmail.com>
2026-07-21 10:50:28 +0530

    Inital Commit

---
+++ SwiftUICICDDemo/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SwiftUICICDDemo
+//
+//  Created by apple on 21/07/26.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+
+        VStack {
+
+            Text("CI CD Demo")
+                .font(.largeTitle)
+
+            Button("Login") {
+                print("Login Clicked")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUICICDDemo/SwiftUICICDDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICICDDemoApp.swift
+//  SwiftUICICDDemo
+//
+//  Created by apple on 21/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICICDDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
